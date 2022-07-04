ed558d82bbe22bd2380b223bad16c63babe9471d
youfoundaclue <ssmeowwow@gmail.com>
2022-07-04 12:41:26 +0100

    u

--- site.js
+++ site.js
@@ -1630,10 +1630,10 @@ function generateCurse() {
 				"You find that you naturally feel a little drunk.",
 				"You have a strange affinity for leaves.",
 				"You have tantalizingly soft fur.",
-				lewdSelected && decidedAndFalse(subjectFemale) ? "Your balls grow larger the longer you go without release, often becoming excessive." : ""]);
+				lewdSelected && decidedAndFalse(subjectFemale) ? "Your balls grow larger the longer you go without release, often becoming excessive." : ""]);,
 			requires: [becomingCreatureHybrid, beastOption],
 			sets: [mundaneAnimalSubject, setExtremitiesName("paws")],
-			},
+		},
 		{
 			makeSubjectText: function(){return isDecided(subjectFemale) ? subjectFemale ? "rabbit doe" : "rabbit buck" : randomFrom(["rabbit", "bunny"]);},
 			chosen: function(){facialFeatureName = randomFrom(["buck teeth", "long ears"]);},
